239eb63158d02389c9318443050d3ac145ec2893
shnepp <oceanserg@yandex.ru>
2019-11-07 15:29:06 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -13,7 +13,7 @@ echo "connection to DB in progress...";
 //Подключаемся к базе
 		$user = 'b7319c85c459b1';
         $pass = '4b13204c';
-        $dbh = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=new_english', $user, $pass);
+        $dbh = new PDO('mysql:host=eu-cdbr-west-02.cleardb.net;dbname=heroku_84caafe9e1164cc', $user, $pass);
         $dbh->exec("set names utf8");
 
         echo "connection succeed";
